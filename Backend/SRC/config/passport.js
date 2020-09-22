95d5fdbd51f3d2b1cd72024d6782a36db9974833
JoacoVial <joaquin.vial@mail.udp.cl>
2020-09-22 17:46:06 -0300

    cambios

--- Backend/SRC/config/passport.js
+++ Backend/SRC/config/passport.js
@@ -39,7 +39,7 @@ const Usuario = require('../app/models/usuario');
         newUsuario.password = newUsuario.generateHash(password);
         newUsuario.save(function (err) {
           if (err) { throw err; }
-          res.sendStatus(201)//done(null, newUsuario);
+          return done(null, newUsuario);
         });
       }
     });
--- Backend/SRC/index.js
+++ Backend/SRC/index.js
@@ -18,7 +18,6 @@ connectDB();
 
 //Setting
 app.set('port', process.env.PORT || 8000);
-app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
