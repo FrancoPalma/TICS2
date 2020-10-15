42bb665addcc70a477d82086f6b4bc05cbf83486
dEmanu1001 <64873829+dEmanu1001@users.noreply.github.com>
2020-10-15 16:04:44 -0300

    jj
    jjj

--- Backend/SRC/app/models/usuario.js
+++ Backend/SRC/app/models/usuario.js
@@ -7,12 +7,10 @@ const usuario = new mongoose.Schema({
   password: {type: String},
   nombre: {type: String},
   telefono: {type: String},
-  privilegios: {
-    gestion_empledo: {type: Boolean, require: true},
-    gestion_inventario: {type: Boolean, require: true},
-    gestion_privilegios: {type: Boolean, require: true},
-    descuento_permitido: {type: Number, require: true}
-  },
+  gestion_empledo: {type: Boolean, require: true},
+  gestion_inventario: {type: Boolean, require: true},
+  gestion_privilegios: {type: Boolean, require: true},
+  descuento_permitido: {type: Number, require: true}  ,
   sucursal: {type: String}},
   { collection : 'usuario'}
 );
