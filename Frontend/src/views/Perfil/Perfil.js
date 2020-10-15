47e44ecf3594d04c018a258393418302db449a2e
Francoco97 <fpalmatrejo@gmail.com>
2020-10-15 14:38:33 -0300

    holi

--- Frontend/src/views/Perfil/Perfil.js
+++ Frontend/src/views/Perfil/Perfil.js
@@ -82,136 +82,39 @@ export default class UserProfile extends React.Component {
  }
 
  CambioPassword() {
-   fetch('/editar_password/', {
-   method: 'POST',
-   headers: {
-       Accept: 'application/json',
-       'Content-Type': 'application/json',
-   },
-   body: JSON.stringify({
-     rut: this.state.perfil.rut,
-     new_pass: this.state.new_pass,
-   })
-   })
-   .then( (response) => {
-       if(response.status === 201) {
-           console.log("Editado correctamente")
-       } else {
-           console.log('Hubo un error')
-       }
-   })
-   .catch((error) => {
-       console.log(error)
-   });
+   console.log(this.state.perfil.rut)
+   if(this.state.new_pass === this.state.conf_pass){
+     fetch('/editar_password/', {
+     method: 'POST',
+     headers: {
+         Accept: 'application/json',
+         'Content-Type': 'application/json',
+     },
+     body: JSON.stringify({
+       rut: this.state.perfil.rut,
+       new_pass: this.state.new_pass,
+     })
+     })
+     .then( (response) => {
+         if(response.status === 201) {
+             console.log("Editado correctamente")
+         } else {
+             console.log('Hubo un error')
+         }
+     })
+     .catch((error) => {
+         console.log(error)
+     });
+   }else{
+     console.log("Las pss no son iguales")
+   }
  }
 
   render() {
     if(this.state.isReady === true) {
       return (
         <GridContainer>
-          {/* <GridItem xs={12} sm={12} md={8}>
-            <Card>
-              <CardHeader color="primary">
-                <h4 style={styles.cardTitleWhite}>Editar perfil</h4>
-                <p style={styles.cardCategoryWhite}>Completa la siguiente informacion para editar tu usuario</p>
-              </CardHeader>
-              <CardBody>
-                <GridContainer>
-                  <GridItem xs={12} sm={12} md={8}>
-                    <CustomInput
-                      labelText="Nombre Usuario"
-                      id="nombre"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                    />
-                  </GridItem>
-                  <GridItem xs={12} sm={12} md={4}>
-                    <CustomInput
-                      labelText="Rut"
-                      id="rut"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                    />
-                  </GridItem>
-                </GridContainer>
-                <GridContainer>
-                  <GridItem xs={12} sm={12} md={6}>
-                    <CustomInput
-                      labelText="Telefono"
-                      id="phone"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                    />
-                  </GridItem>
-                </GridContainer>
-                <GridContainer>
-                  <GridItem xs={12} sm={12} md={6}>
-                    <GridContainer>
-                      <GridItem xs={12} sm={12} md={4}>
-                        <CustomInput
-                          labelText="Dia"
-                          id="dia"
-                          formControlProps={{
-                            fullWidth: true
-                          }}
-                        />
-                      </GridItem>
-                      <GridItem xs={12} sm={12} md={4}>
-                        <CustomInput
-                          labelText="Mes"
-                          id="mes"
-                          formControlProps={{
-                            fullWidth: true
-                          }}
-                        />
-                      </GridItem>
-                      <GridItem xs={12} sm={12} md={4}>
-                        <CustomInput
-                          labelText="Año"
-                          id="ano"
-                          formControlProps={{
-                            fullWidth: true
-                          }}
-                        />
-                      </GridItem>
-                    </GridContainer>
-                    <InputLabel style={{ color: "#AAAAAA" }}>Fecha de nacimiento</InputLabel>
-                  </GridItem>
-                  <GridItem xs={12} sm={12} md={3}>
-                    <CustomInput
-                      labelText="Salario"
-                      id="salario"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        disabled: true
-                      }}
-                    />
-                  </GridItem>
-                  <GridItem xs={12} sm={12} md={3}>
-                    <CustomInput
-                      labelText="Sucursal"
-                      id="sucursal"
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        disabled: true
-                      }}
-                    />
-                  </GridItem>
-                </GridContainer>
-              </CardBody>
-              <CardFooter>
-                <Button color="primary">Actualizar</Button>
-              </CardFooter>
-            </Card>
-          </GridItem> */}
-          <GridItem xs={12} sm={12} md={12} style={{display: 'flex',  justifyContent:'center', height: '50vh'}}>
+          <GridItem xs={12} sm={12} md={12} style={{display: 'flex', margin:'100', justifyContent:'center', height: '50vh'}}>
             <Card profile style = {{width: 580, height: 400}}>
               <CardAvatar profile>
                 <a href="#pablo" onClick={e => e.preventDefault()}>
@@ -281,9 +184,9 @@ export default class UserProfile extends React.Component {
                 <TextField
                   variant="outlined"
                   required
-                  type="new_pass"
                   name="new_pass"
                   label="Nueva Contraseña"
+                  type="password"
                   id="new_pass"
                   onChange={(event) => this.setState({new_pass:event.target.value})}
                 />
@@ -292,7 +195,7 @@ export default class UserProfile extends React.Component {
                 <TextField
                   variant="outlined"
                   required
-                  type="conf_pass"
+                  type="password"
                   name="conf_pass"
                   label="Confirmar Contraseña"
                   id="conf_pass"
