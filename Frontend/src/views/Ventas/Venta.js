bb8481dda9f2a5e3195725221f428482a9570c19
Francoco97 <fpalmatrejo@gmail.com>
2020-11-24 17:40:27 -0300

    Update Venta.js

--- Frontend/src/views/Ventas/Venta.js
+++ Frontend/src/views/Ventas/Venta.js
@@ -732,7 +732,7 @@ export default class Ventas extends React.Component {
                           <MaterialTable
                               title={nombresucursal}
                               options={{filtering: true}}
-                              columns={ [{ title: 'Numero', field: 'numero_venta', type: 'numeric'},
+                              columns={ [{ title: 'Numero', field: 'numero', type: 'numeric'},
                                         {title: 'Producto', field: 'cod_prod'},
                                         { title: 'Fecha', field: 'fecha', type: 'date'},
                                         {title: 'Total', field:'valor_prod'}]}
@@ -860,7 +860,7 @@ export default class Ventas extends React.Component {
                         <MaterialTable
                             title={nombresucursal}
                             options={{filtering: true}}
-                            columns={ [{ title: 'Numero', field: 'numero_venta', type: 'numeric'},
+                            columns={ [{ title: 'Numero', field: 'numero', type: 'numeric'},
                                       {title: 'Producto', field: 'cod_prod'},
                                       { title: 'Fecha', field: 'fecha', type: 'date'},
                                       {title: 'Total', field:'valor_prod'}]}
